8cea98780ad113608be3745e1a6b8096f7924ab9
Gayathiri Balakrishnan <bgayathiri231@gmail.com>
2023-11-29 15:48:41 +0530

    Implented Cmponent

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from "./components/input/Input";
+import Content from "./components/content/Content";
+import Button from './components/button/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <Input type="email" placeholder={"Email address"} size={"largeInput"} />
+      <Input
+        type="email"
+        placeholder={"Email or Phone number"}
+        size={"smallInput"}
+      />
+      <Input type="password" placeholder={"Password"} size={"smallInput"} />
+      <Content
+        heading="The biggest Indian hits. The best Indian stories. All streaming here."
+        text="Watch anywhere. Cancel anytime."
+        para="Ready to watch? Enter your email to create or restart your membership."
+      />
+      <Content
+        heading="Enjoy on your TV"
+        text="Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more."
+      />
+      <Content
+        heading="Download your shows to watch offline"
+        text="Save your favourites easily and always have something to watch."
+      />
+      <Content
+        heading="Watch everywhere"
+        text="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+      />
+      <Content
+        heading="Create profiles for kids"
+        text="Send children on adventures with their favourite characters in a space made just for them—free with your membership."
+      />
+      <Button text="Sign in" size="small"/>
     </div>
   );
 }
